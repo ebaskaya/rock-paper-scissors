c961b54618d36aa2f79f074f1776332a053de580
ebaskaya <egemen.baskaya@outlook.com>
2022-11-12 17:40:42 +0300

    Get computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+
+
+
+function getComputerChoice() {
+    const computerChoices = ["Rock", "Paper", "Scissors"];
+    let computerChoice = Math.floor(Math.random() * 3);
+    return computerChoices[computerChoice];
+}
+
+computerChoice = getComputerChoice();
+console.log(computerChoice);
