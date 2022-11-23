5bdfd2e3cc2df47aefb69c5b7bf7a3cef0d9621d
ebaskaya <egemen.baskaya@outlook.com>
2022-11-23 14:27:23 +0300

    add score system

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+let playerScore = 0;
+let computerScore = 0;
+
 const buttons = document.querySelectorAll('.choice');
 const result = document.createElement('div');
 const scores = document.createElement('div');
+scores.textContent = "Player: 0 Computer: 0";
+document.body.appendChild(scores);
 document.body.appendChild(result);
 
 
 buttons.forEach(button => button.addEventListener('click', setChoices));
 
-let playerScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice(choices) {
     
@@ -18,12 +22,16 @@ function getComputerChoice(choices) {
 
 function playRound(playerChoice, computerChoice, choices){
     if(playerChoice === computerChoice) {
-        result.textContent = 'It\'s a draw!'; 
+        result.textContent = "It's a draw!"; 
     }
     else if(choices[(choices.indexOf(playerChoice) + 1) % 3] === computerChoice) {
-        result.textContent = `You win! ${playerChoice} beats ${computerChoice}.`; 
+        result.textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
+        scores.textContent =  `Player: ${++playerScore} Computer: ${computerScore}`;
     }
-    else result.textContent = `You lose! ${playerChoice} loses to ${computerChoice}.`; 
+    else {
+        result.textContent = `You lose! ${playerChoice} loses to ${computerChoice}.`;
+        scores.textContent = `Player: ${playerScore} Computer: ${++computerScore}`
+        } 
 }
 
 
@@ -35,43 +43,14 @@ function setChoices() {
     
     playRound(playerChoice, computerChoice, choices);
 
-    // if(playerChoice === computerChoice) {
-    //     return 0; 
-    // }
-    // else if(choices[(choices.indexOf(playerChoice) + 1) % 3] === computerChoice) {
-    //     return 1; 
-    // }
-    // else return -1; 
+    
     
     
     
     
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        computerChoice = getComputerChoice();
-        
-        playerChoice = getPLayerChoice();
-        
-        result = playRound(computerChoice, playerChoice);
-        if (result === 1) {
-            console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
-            playerScore++;
-        }
-        else if (result === 0){
-            console.log("It's a tie!")
-        }
-        else {
-            console.log(`You lose! ${playerChoice} loses to ${computerChoice}.`)
-            computerScore++;
-        }
-        
-    }
-    console.log(`Final score is Player: ${playerScore} Computer: ${computerScore}`);
-}
+
 
 //game();
 
