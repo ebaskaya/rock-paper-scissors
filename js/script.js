e9d6517bbcfd9f706280203594655a27e935ebdf
ebaskaya <egemen.baskaya@outlook.com>
2022-11-23 14:43:28 +0300

    Make game end after a player reaches 5

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let computerScore = 0;
 const buttons = document.querySelectorAll('.choice');
 const result = document.createElement('div');
 const scores = document.createElement('div');
+let game = true;
 scores.textContent = "Player: 0 Computer: 0";
 document.body.appendChild(scores);
 document.body.appendChild(result);
@@ -21,6 +22,8 @@ function getComputerChoice(choices) {
 
 
 function playRound(playerChoice, computerChoice, choices){
+    
+
     if(playerChoice === computerChoice) {
         result.textContent = "It's a draw!"; 
     }
@@ -31,11 +34,21 @@ function playRound(playerChoice, computerChoice, choices){
     else {
         result.textContent = `You lose! ${playerChoice} loses to ${computerChoice}.`;
         scores.textContent = `Player: ${playerScore} Computer: ${++computerScore}`
-        } 
+        }
+    if(playerScore === 5 || computerScore === 5){
+        
+        result.textContent = `Game is finished. ${playerScore > computerScore ? 'Player wins' : 'Computer wins.'}`
+        game = false;
+        
+    }
+    
 }
 
 
 function setChoices() {
+    if(!game){
+        return;
+    }
     const choices = ["Rock", "Scissors", "Paper"];
     playerChoice = this.textContent; //button's text content
     computerChoice = getComputerChoice(choices);
@@ -44,13 +57,9 @@ function setChoices() {
     playRound(playerChoice, computerChoice, choices);
 
     
-    
-    
-    
-    
 }
 
 
 
-//game();
+
 
